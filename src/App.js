146080e8b1d3cab087178e81479cb33ab105bc4a
nouria chaouy <nouria.chaouy@gmail.com>
2022-08-10 10:02:30 +0100

    first commit - modified file structure and added npm packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+let username;
+
+const url = `https://api.github.com/users/${username}/repos`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Lap 3 Code Challenge</h2>
     </div>
   );
 }
