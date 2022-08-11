bdeb6d86e137d3ab0c036acd5043af39d169ba8f
Stefan <stefan.d.yordanov@gmail.com>
2022-08-11 09:50:58 +0100

    Updated the home page styling

--- src/components/SingleRepo/SingleRepo.js
+++ src/components/SingleRepo/SingleRepo.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./SingleRepo.css";
 
 export default function SingleRepo({ repo }) {
-  return (
-    <article className="repo">
-      <header>
-        <h4>{repo.name}</h4>
-        <button className="btn">See Details</button>
-      </header>
-    </article>
-  );
+    return (
+        <article className="repo">
+            <header>
+                <h4 className="repo-name">{repo.name}</h4>
+                <button className="btn">See Details</button>
+            </header>
+        </article>
+    );
 }
